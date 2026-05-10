fd8f10df24c7b341f8f7cd93767daa2bdf621d03
Chong <Tp082747@apu.mail.edu.my>
2026-05-10 15:57:39 +0800

    New Updates

---
+++ pages/auth/login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APCampusHub</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../assets/css/auth/login.css">
+</head>
+<body>
+    
+    <div class="overlay">
+
+        <!-- LOGO -->
+        <div class="top-section">
+
+            <img src="../../assets/images/logo.png" alt="Logo" class="logo">
+
+            <h1>APCampusHub</h1>
+
+            <p class="slogan">An All-in-one Campus Management Application</p>
+
+        </div>
+
+        <!-- Login Box -->
+        <div class="login-container">
+
+            <form action="login_process.php" method="POST">
+
+                <div class="input-group">
+
+                    <input type="text" name="username" placeholder="TP Number" required>
+
+                </div>
+
+                <div class="input-group">
+
+                    <input type="password" name="password" placeholder="Password" required>
+
+                </div>
+
+                <!-- Forget Password Function -->
+                <div class="forget-password">
+
+                    <a href="#">
+
+                        Forget Password?
+
+                    </a>
+
+                </div>
+
+                <!-- Submit Button -->
+                <button type="submit" name="loginBtn">
+
+                    Login
+
+                </button>
+
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ pages/auth/login_process.php
@@ -0,0 +1,113 @@
+<?php
+
+session_start();
+require_once("../../config/db.php");
+
+if(!isset($_SESSION['login_attempts']))
+{
+    $_SESSION['login_attempts'] = 0;
+}
+
+if(!isset($_SESSION['lock_time'])) {
+    $_SESSION['lock_time'] = 0;
+}
+
+if($_SESSION['lock_time'] > time())
+{
+    $remaining = $_SESSION['lock_time'] - time();
+
+    echo "<script>alert('Too many attempts. Please wait $remaining seconds.'); window.location.href='login.php'; </script>";
+    exit();
+}
+
+if(isset($_POST['loginBtn']))
+{
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    if(empty($username) || empty($password)) {
+        echo "<script>alert('Please fill in all fields.'); window.location.href='login.php';</script>";
+        exit();
+    }
+
+    $sql = "SELECT * FROM user WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+
+    if(!$stmt){
+        die("Prepare failed: " . $conn->error);
+    }
+
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+
+        if(password_verify($password, $user['password'])) {
+            $_SESSION['login_attempts'] = 0;
+            $_SESSION['lock_time'] = 0;
+
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+
+            switch($user['role']) {
+                case "student":
+                    header("Location: ../student/dashboard.php");
+                    break;
+                
+                case "lecturer":
+                    header("Location: ../lecturer/dashboard.php");
+                    break;
+
+                case "admin":
+                    header("Location: ../admin/dashboard.php");
+                    break;
+
+                case "staff":
+                    header("Location: ../staff/dashboard.php");
+                    break;
+
+                default:
+                    echo "<script>alert('Invalid role'); window.location.href='login.php';</script>";
+                    exit();
+            }
+        } else {
+            $_SESSION['login_attempts']++;
+
+            if($_SESSION['login_attempts'] >= 3) {
+                $_SESSION['login_attempts'] = 0;
+                $_SESSION['lock_time'] = time() + 10;
+
+                echo "<script>alert('Too many failed attempts. Please wait 10 seconds.'); window.location.href='login.php'; </script>";
+                exit();
+            }
+
+            $remain = 3 - $_SESSION['login_attempts'];
+
+            echo "<script>alert('Wrong password. Attempts left: $remain'); window.location.href='login.php';</script>";
+            exit();
+        }
+    } else {
+        $_SESSION['login_attempts']++;
+
+        if($_SESSION['login_attempts'] >= 3) {
+                $_SESSION['login_attempts'] = 0;
+                $_SESSION['lock_time'] = time() + 10;
+
+                echo "<script>alert('Too many failed attempts. Please wait 10 seconds.'); window.location.href='login.php'; </script>";
+                exit();
+            }
+
+        $remain = 3 - $_SESSION['login_attempts'];
+
+        echo "<script>alert('User not found. Attempts left: $remain'); window.location.href='login.php';</script>";
+        exit();
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
